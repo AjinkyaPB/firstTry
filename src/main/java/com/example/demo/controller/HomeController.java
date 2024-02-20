d217e36f08d95c872a1414be890b76076909eb17
AjinkyaPB <bhawalajinkya@live.in>
2024-02-20 17:58:08 +0530

    First Commit

---
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -0,0 +1,52 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+	
+	
+	@RequestMapping("/index")
+	public String defaultPage() {
+		return"index";
+	}
+
+	@RequestMapping("/about")
+	public String aboutPage()
+	{
+		return "about";
+		
+	}
+	@RequestMapping("/service")
+	public String ServicePage()
+	{
+		return "service";
+		
+	}
+	@RequestMapping("/contact")
+	public String contactPage()
+	{
+		return "contact";
+		
+	}
+	
+	@PostMapping(path="/buyingPage")
+	public String BuyerPage()
+	{
+		return "buyingPage";
+
+	}
+	
+	@PostMapping(path="/SellingPage")
+	public String SellerPage()
+	{
+		return "SellingPage";
+
+	}
+	
+	
+
+}
